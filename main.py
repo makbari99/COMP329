44b05c34a2a90aa086589cb01ae861eefcf0f859
makbari99 <makbari@luc.edu>
2021-04-28 18:33:44 -0500

    Logistoc Regression added

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+nltk.download()
 import string
+from sklearn.linear_model import LogisticRegression
 
 ### Read training file and testing file
 df_train = pd.read_csv("Corona_NLP_train.csv", encoding='ISO-8859-1')
@@ -12,28 +14,28 @@ print(df_test.shape)
 
 ### Narrow our classification to three classes: Positive, Neutral, Negative and drop the unnecessary column
 df_train = df_train.replace(regex={'Extremely Positive': 'Positive', 'Extremely Negative': 'Negative'})
-print(pd.unique(df_train['Sentiment']) )
-df_train = df_train.drop(['UserName','ScreenName','Location','TweetAt'], axis=1)
+print(pd.unique(df_train['Sentiment']))
+df_train = df_train.drop(['UserName', 'ScreenName', 'Location', 'TweetAt'], axis=1)
 
 df_test = df_test.replace(regex={'Extremely Positive': 'Positive', 'Extremely Negative': 'Negative'})
-print(pd.unique(df_test['Sentiment']) )
-df_test = df_test.drop(['UserName','ScreenName','Location','TweetAt'], axis=1)
-
+print(pd.unique(df_test['Sentiment']))
+df_test = df_test.drop(['UserName', 'ScreenName', 'Location', 'TweetAt'], axis=1)
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
 
 ### Text Cleaning
 def clean_text(df):
     all_reviews = list()
     lines = df["OriginalTweet"].values.tolist()
     for text in lines:
-        #Lower Casing
-        text = text.lower()        
-        
+        # Lower Casing
+        text = text.lower()
+
         # Remove numbers, URL and punctuation
-        text = re.sub(r'\d+', '', text)        
+        text = re.sub(r'\d+', '', text)
         pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
         text = pattern.sub('', text)
 
@@ -41,21 +43,22 @@ def clean_text(df):
         table = str.maketrans('', '', string.punctuation)
         stripped = [w.translate(table) for w in tokens]
         words = [word for word in stripped if word.isalpha()]
-        
-        #Remove stopwords
+
+        # Remove stopwords
         stop_words = set(stopwords.words("english"))
         stop_words.discard("not")
         words = [w for w in words if not w in stop_words]
         words = ' '.join(words)
-        
+
         # lemmatize string
         lemmatizer = WordNetLemmatizer()
         word_tokens = word_tokenize(text)
-        text = [lemmatizer.lemmatize(word, pos ='v') for word in word_tokens]
-        
+        text = [lemmatizer.lemmatize(word, pos='v') for word in word_tokens]
+
         all_reviews.append(words)
-        
+
     return all_reviews
+
 
 ###Split to testing set and development set
 from sklearn.model_selection import train_test_split
@@ -77,8 +80,19 @@ x_dev_vec = vectorizer.transform(x_dev).toarray()
 ###Using LinearSVC model for predictions
 from sklearn.svm import LinearSVC
 from sklearn import metrics
+
 clf = LinearSVC()
 clf.fit(x_train_vec, y_train)
 prediction = clf.predict(x_dev_vec)
 print("The prediction accuracy of the development set with LinearSVC model is:")
 print(metrics.accuracy_score(prediction, y_dev))
+
+## Logistic Regression
+model_LR = LogisticRegression()
+model_LR.fit(x_train, y_train)
+
+y_predict_LR = model_LR.predict(x_test)
+model_LR_score = model_LR.score(x_test, y_test)
+
+print("-------Logistic Regression-------")
+print("Accuracy: ", model_LR_score*100)
